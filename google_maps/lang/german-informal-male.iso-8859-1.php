3f2dbcc5008eb4639613038869dd84a74adfe950
oricgn <oliver@riesen.org>
2015-08-05 14:22:44 +0200

    Correcting handling for https; new german language files

---
+++ google_maps/lang/german-informal-male.iso-8859-1.php
@@ -0,0 +1,18 @@
+<?php
+
+// This file ist part of the German Language Files Package
+// Get the complete package here:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+// Diese Datei ist Teil des Deutschen Sprachpakets
+// Das komplette Paket gibt es hier:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+include(str_replace('-informal-male', '', __FILE__));
+
+$PHORUM['DATA']['LANG']['mod_google_maps']['CCIntroduction']      = 'Du kannst in der Karte Deinen Standort (Wohnort) markieren. Die Markierung kannst Du mit einem Doppelklick setzen und mit der Maus verschieben. Alternativ kannst Du auch die Suche benutzen.';
+$PHORUM['DATA']['LANG']['mod_google_maps']['IncompatibleBrowser'] = 'Entschuldige, Dein Browser ist inkompatibel. Die für Google Maps benötigten Funktionen fehlen.';
+$PHORUM['DATA']['LANG']['mod_google_maps']['NoSearchResults']     = 'Entschuldige, es wurde kein Ergebnis für Deine Suchanfrage gefunden.';
+$PHORUM['DATA']['LANG']['mod_google_maps']['YourLocation']        = 'Dein Standort';
+
+?>
---
+++ google_maps/lang/german.utf-8.php
@@ -0,0 +1,31 @@
+<?php
+
+// This file ist part of the German Language Files Package
+// Get the complete package here:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+// Diese Datei ist Teil des Deutschen Sprachpakets
+// Das komplette Paket gibt es hier:
+// http://www.phorum.org/phorum5/read.php?65,129717
+
+$PHORUM['DATA']['LANG']['mod_google_maps'] = array
+(
+    'CCIntroduction'      => 'Sie können in der Karte Ihren Standort (Wohnort) markieren. Die Markierung können Sie mit einem Doppelklick setzen und mit der Maus verschieben. Alternativ können Sie auch die Suche benutzen.',
+    'CCMenuItem'          => 'Standort',
+    'Clear'               => 'Standort löschen',
+    'GetMyLocation'       => 'Meinen aktuellen Standort verwenden',
+    'IncompatibleBrowser' => 'Entschuldigen Sie, Ihr Browser ist inkompatibel. Die für Google Maps benötigten Funktionen fehlen.',
+    'Location'            => 'Standort',
+    'NoLocationSet'       => 'Kein Standort ausgewählt.',
+    'NoSearchResults'     => 'Entschuldigen Sie, es wurde kein Ergebnis für Ihre Suchanfrage gefunden.',
+    'ProfileTitle'        => 'Standort dieses Teilnehmers:',
+    'Search'              => 'Standort suchen',
+    'YourLocation'        => 'Ihr Standort',
+    // usermap addon
+    'UserMapTitle'        => 'Standorte unserer Teilnehmer',
+    'UserMapNoUsers'      => 'Leider sind noch keine Standorte vorhanden.',
+    // Language for reverse geocoding (retrieving the name of the city)
+    'geocoding_lang'      => 'de'
+);
+
+?>
